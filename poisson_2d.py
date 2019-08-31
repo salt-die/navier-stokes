a431fd4579da48a73cc6a1c475b794057adeaf72
salt-die <naclmolecule@gmail.com>
2019-08-31 13:21:42 -0500

    simplified

--- poisson_2d.py
+++ poisson_2d.py
@@ -46,9 +46,9 @@ class Display(Widget):
         return True
 
     def update(self, dt):
-        kernel = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+        kernel = np.array([[0, .25, 0], [.25, 0, .25], [0, .25, 0]])
         #Just laplace with a relaxing term
-        self.poisson_2d = (nd.convolve(self.poisson_2d, kernel, mode='wrap') - .01)/ 4
+        self.poisson_2d = nd.convolve(self.poisson_2d, kernel, mode='wrap') - .01
 
         self.texture.blit_buffer(np.dstack([np.zeros(texture_dim,
                                                dtype=np.float32)] * 2 +\
